80eb82a5750b60596220e18de919f273183f8829
jeffersonTrendx <ojefdev@gmail.com>
2025-09-03 22:39:15 -0300

    Fluxx#20-Focar-a-rolagem-da-lista-de-meses-para-o-mês-atual
    Adicionado a ação de focar a lista no Mês atual

--- lib/pages/month_list_page.dart
+++ lib/pages/month_list_page.dart
@@ -3,12 +3,14 @@ import 'package:Fluxx/blocs/months_list_bloc/months_list_state.dart';
 import 'package:Fluxx/components/app_bar.dart';
 import 'package:Fluxx/components/month.dart';
 import 'package:Fluxx/components/shimmers/months_shimmer.dart';
+import 'package:Fluxx/models/month_model.dart';
 import 'package:Fluxx/themes/app_theme.dart';
 import 'package:Fluxx/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:intl/intl.dart';
 
 class MonthListPage extends StatefulWidget {
   const MonthListPage({super.key});
@@ -26,8 +28,21 @@ class _MonthListPageState extends State<MonthListPage> {
     super.initState();
   }
 
+  void jumpToCurrentMonth(List<MonthModel>months, Size mediaQuery) {
+    final currentMonthName = DateFormat.MMMM('pt_BR').format(DateTime.now());
+    final currentMonthIndex = months.indexWhere((m) => m.name!.toLowerCase() == currentMonthName.toLowerCase());
+    final monthHeight = mediaQuery.height * .2;
+
+    if (currentMonthIndex != -1) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _pageScrollController.jumpTo(currentMonthIndex * monthHeight);
+    });
+  }
+  }
+
   @override
   Widget build(BuildContext context) {
+    var mediaQuery = MediaQuery.of(context).size;
     return AnnotatedRegion(
       value: SystemUiOverlayStyle.dark,
       child: Scaffold(
@@ -52,6 +67,7 @@ class _MonthListPageState extends State<MonthListPage> {
                         child: Center(child: Text('Erro ao carregar meses.')),
                       );
                     } else {
+                      jumpToCurrentMonth(state.months, mediaQuery);
                       return Column(
                         children: [
                           Text('${DateTime.now().year}',style: AppTheme.textStyles.titleTextStyle,),
